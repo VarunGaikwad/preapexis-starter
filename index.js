5594959241f9e26bb5eeebc925f9d8c9c4b61216
Varun Gaikwad <gaikwadvarun23@gmail.com>
2024-09-12 15:04:01 +0530

    second

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import readline from "readline";
 import path from "path";
 import { fileURLToPath } from "url";
 import fse from "fs-extra";
-import { isValidProjectName, sucessLog, warnLog } from "./util/util.js";
+import { isValidProjectName, sucessLog, errorLog } from "./util/util.js";
 
 const __filename = fileURLToPath(import.meta.url),
   __dirname = path.dirname(__filename),
@@ -13,40 +13,35 @@ const __filename = fileURLToPath(import.meta.url),
   currentPath = cwd(),
   sampleFolderPath = path.join(__dirname, "sample");
 
-if (argv.length === 2) {
-  const rl = readline.createInterface({
-    input: stdin,
-    output: stdout,
-  });
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
 
-  rl.question("Enter your project name: ", async function (name) {
-    if (!isValidProjectName(name)) {
-      errorLog("Invalid project name");
-      rl.close();
-      exit(1);
-    }
+rl.question("Enter your project name: ", async function (name) {
+  if (!isValidProjectName(name)) {
+    errorLog("Invalid project name");
+    rl.close();
+    exit(1);
+  }
 
-    const newFolderPath = path.join(currentPath, name),
-      packageJsonPath = path.join(newFolderPath, "package.json");
+  const newFolderPath = path.join(currentPath, name),
+    packageJsonPath = path.join(newFolderPath, "package.json");
 
-    try {
-      await fse.ensureDir(newFolderPath);
-      await fse.copy(sampleFolderPath, newFolderPath);
+  try {
+    await fse.ensureDir(newFolderPath);
+    await fse.copy(sampleFolderPath, newFolderPath);
 
-      const packageJson = await fse.readJson(packageJsonPath);
-      packageJson.name = name;
-      await fse.writeJson(packageJsonPath, packageJson, { spaces: 2 });
-      sucessLog(
-        `Project created successfully 🌱!\n cd ${name} \n npm install \n npm run dev`
-      );
-    } catch (error) {
-      errorLog(`Error: ${error.message}`);
-      exit(1);
-    } finally {
-      rl.close();
-    }
-  });
-} else {
-  warnLog("Usage: npm run <command>");
-  exit(1);
-}
+    const packageJson = await fse.readJson(packageJsonPath);
+    packageJson.name = name;
+    await fse.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+    sucessLog(
+      `Project created successfully 🌱!\n cd ${name} \n npm install \n npm run dev`
+    );
+  } catch (error) {
+    errorLog(`Error: ${error.message}`);
+    exit(1);
+  } finally {
+    rl.close();
+  }
+});
--- util/util.js
+++ util/util.js
@@ -1,12 +1,12 @@
 import chalk from "chalk";
 
-const log = console.log;
-function isValidProjectName(name) {
-  const regex = /^[a-z0-9-]+$/;
-  return regex.test(name);
-}
-
-const sucessLog = (message) => log(chalk.green(message)),
+const log = console.log,
+  isValidProjectName = (name) => {
+    const regex =
+      /^(?:(?:@(?:[a-z0-9-*~][a-z0-9-*._~]*)?\/[a-z0-9-._~])|[a-z0-9-~])[a-z0-9-._~]*$/;
+    return regex.test(name);
+  },
+  sucessLog = (message) => log(chalk.green(message)),
   errorLog = (message) => log(chalk.red(message)),
   warnLog = (message) => log(chalk.yellow(message)),
   infoLog = (message) => log(chalk.blue(message));
